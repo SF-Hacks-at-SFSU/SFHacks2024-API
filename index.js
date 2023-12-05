b7e31fe4809a59de63dfc863e9d811bf781bcc92
Odera Nwosu <oderanwosu.j@gmail.com>
2023-12-04 18:02:03 -0800

    Added webhook/send-received-app-email route

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
-import path from 'path';
-import express from 'express';
+//imports
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+//get enviroment configuration
+dotenv.config();
 
-app.listen(port, () => {
-    console.log(`Listening on http://localhost:${port}/`);
+const PORT = process.env.API_KEY;
+var app = express();
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on http://localhost:${process.env.PORT}/`);
+});
+
+app.post("webhook/send-received-app-email", (req, res) => {
+  const webhookPayload = req.body;
+  const receivedSignature = req.headers["tally-signature"];
+
+  const calculatedSignature = createHmac(
+    "sha256",
+    process.env.TALLY_SIGNING_SECRET
+  )
+    .update(JSON.stringify(webhookPayload))
+    .digest("base64");
+
+  // Compare the received signature with the calculated signature
+  if (receivedSignature !== calculatedSignature) {
+     // Signature is invalid, reject the webhook request
+     res.status(401).send("Invalid signature.");
+     return;
+  }
+  
+
+
+
+  
 });
