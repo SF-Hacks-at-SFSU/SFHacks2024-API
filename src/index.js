a0629ae5627bce64400c33853329c949c6e12303
Odera Nwosu <oderanwosu.j@gmail.com>
2023-12-04 21:11:04 -0800

    Added email sending to API route

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import { createHmac } from "crypto";
+import { sendConfirmedReceivedEmail } from "./send_email";
 
 //get enviroment configuration
 dotenv.config();
@@ -17,27 +18,36 @@ app.listen(process.env.PORT, () => {
 
 //Sends an email confirming that the recepients application has been sent. Only Tally.so webhooks should be allowed to access the route.
 app.post("/webhook/send-received-app-email", (req, res) => {
-    console.log("Received Tally.so event:", req.url )
-  const webhookPayload = req.body;
-  const receivedSignature = req.headers["tally-signature"];
+try {
+      console.log("Received Tally.so event:", req.url )
+    
+    
+    const webhookPayload = req.body;
+    const receivedToken = req.headers["api-token"];
+    // Compare the received token with the calculated token.
+    //TODO implement HMac function
+    if (!(receivedToken === process.env.TALLY_SIGNING_SECRET)) res.status(401).send('Invalid signature.');
   
-
-  const calculatedSignature = createHmac(
-    "sha256",
-    process.env.TALLY_SIGNING_SECRET
-  )
-    .update(JSON.stringify(webhookPayload))
-    .digest("base64");
-
-  // Compare the received signature with the calculated signature.
-  if (receivedSignature === calculatedSignature) {
-    // Signature is valid, process the webhook payload
-    res.status(200).send('Webhook received and processed successfully.');
-    console.log(webhookPayload)
-  } else {
-    // Signature is invalid, reject the webhook request
-    res.status(401).send('Invalid signature.');
-  }
-
+    let email =  "";
+    let receipient = ""
+    for (const field of webhookPayload['data']['fields']) {
+      if (field["key"]== "question_2Eylpp") {
+        email = field["value"]
+       
+      }
+      if (field["key"]== "question_rj1JeM") {
+        receipient = field["value"]
+       
+      }
+    }
   
+    sendConfirmedReceivedEmail(receipient, email)
+    
+  
+  
+    
+} catch (error) {
+  console.log("Error:", error)
+  res.statusCode(500)
+}
 });
