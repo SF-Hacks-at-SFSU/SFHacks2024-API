8a79be0d4442a5385a99b69bb1c5f70a6d731e63
Odera Nwosu <oderanwosu.j@gmail.com>
2023-12-07 02:53:46 -0800

    Fixed sha56 signature, cleaned comments, organized dir, and added email sending

--- src/index.js
+++ src/index.js
@@ -3,64 +3,66 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import { createHmac } from "crypto";
-import { sendConfirmedReceivedEmail } from "./send_email.js";
+import { sendConfirmedReceivedEmail } from "./utils/send_email.js";
 import bodyParser from "body-parser";
-
+import { verifySignature } from "./utils/verification.js";
+import chalk from "chalk";
 
 //get enviroment configuration
 dotenv.config();
 
-const PORT = process.env.API_KEY;
+const PORT = process.env.PORT || 1000;
 var app = express();
-app.use(bodyParser.json({limit: '50mb'}));  
+//middleware
+app.use(express.raw({ type: "application/json", limit: "50mb" }));
 
+//logging of all request
+app.use((req, res, next) => {
+  const timestamp = new Date().toLocaleString();
+  const method = req.method;
+  const url = req.url;
+  const ipAddress = req.ip || req.connection.remoteAddress;
 
-app.listen(process.env.PORT, () => {
-  console.log(`Listening on http://localhost:${process.env.PORT}/`);
+  console.log(
+    chalk.yellow(`[${timestamp}] ${method} ${url} from ${ipAddress}`)
+  );
+
+  next()
+  
 });
 
 //Sends an email confirming that the recepients application has been sent. Only Tally.so webhooks should be allowed to access the route.
-app.post("/webhook/send-received-app-email", async (req, res) => {
-try {
-    console.log("Received Typeform event:", req.url )
-    const payload = req.body;
-    const receivedSignature = req.headers['Typeform-Signature']
-    
-  
-    const calculatedSignature = createHmac(
-      "sha256",
-      process.env.TYPEFORM_SIGNING_SECRET
-    )
-      .update(JSON.stringify(payload))
-      .digest("base64");
-  
-    // Compare the received signature with the calculated signature.
-    if (!(`sha256=${calculatedSignature}` === receivedSignature)) {
-      // Signature is valid, process the webhook payload
-      res.sendStatus(401)
-      return
-    } 
+app.post("/webhook/send-registration-application-email", async (req, res) => {
+  try {
+    const payload = JSON.parse(req.body.toString());
+    const signature = req.headers["typeform-signature"];
+    const isValid = verifySignature(signature, req.body.toString());
 
-    console.log(webhookPayload)
-    // let fname =  "";
-    //   let receipient = ""
-    //   for (const field of webhookPayload['data']['form_response']["definition"]["fields"]) {
-    //     if (field["id"]== "") {
-    //       receipient = field["value"]
-         
-    //     }
-    //     if (field["key"]== "question_rj1JeM") {
-    //       fname = field["value"]
-         
-    //     }
-    //   }
-    
-    //   await sendConfirmedReceivedEmail(receipient, fname)
-      res.sendStatus(200)
-  
-    
-} catch (error) {
-  res.sendStatus(500)
-  console.log(error)
-}
+    if (!isValid) {
+      // Signature is not valid, bounce back unauthorized
+      res.sendStatus(401);
+      return;
+    }
+
+    var fName = "";
+    var receipient = "";
+    for (const answer of payload["form_response"]["answers"]) {
+      //get email by checking entries email ID
+      if (answer["field"]["id"] == "JV6k42Qo04CQ") receipient = answer["email"];
+
+      //get first name by checking entries id
+      if (answer["field"]["id"] == "TVV0PNJZg9Mz") fName = answer["text"];
+    }
+
+    //Sends email of confirmation
+    await sendConfirmedReceivedEmail(receipient, fName);
+    res.sendStatus(200);
+  } catch (error) {
+    res.sendStatus(500);
+    console.log(error);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(chalk.green(`Server is running on port ${PORT} `));
 });
---
+++ src/utils/verification.js
@@ -0,0 +1,16 @@
+import { createHmac } from "crypto";
+
+
+/**
+ * @description Verifies the receieved signature with the Typeform signing secret assigned to the webhook. If the webhook signature is verifiable, the function will return true
+ * @see https://www.typeform.com/developers/webhooks/secure-your-webhooks/
+ * @param {String} receivedSignature
+ * @param {String} payload
+ * @returns {boolean}
+ */
+export const verifySignature = (receivedSignature, payload) => {
+    const hash = createHmac("sha256", process.env.TYPEFORM_SIGNING_SECRET)
+      .update(payload)
+      .digest("base64");
+    return receivedSignature === `sha256=${hash}`;
+  };
